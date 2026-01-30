fed23f8fb2d1946cf07f4ecc8f74f126dbac9eeb
wscstrive <scwaang@gmail.com>
2026-01-30 20:09:20 +0900

    original 2dgs w/0 depth and normal

--- scripts/dtu_eval.py
+++ scripts/dtu_eval.py
@@ -1,7 +1,7 @@
 import os
 from argparse import ArgumentParser
 
-dtu_scenes = ['scan24'] #, 'scan37', 'scan40', 'scan55', 'scan63', 'scan65', 'scan69', 'scan83', 'scan97', 'scan105', 'scan106', 'scan110', 'scan114', 'scan118', 'scan122']
+dtu_scenes = ['scan24', 'scan37', 'scan40', 'scan55', 'scan63', 'scan65', 'scan69', 'scan83', 'scan97', 'scan105', 'scan106', 'scan110', 'scan114', 'scan118', 'scan122']
 
 parser = ArgumentParser(description="Full evaluation script parameters")
 parser.add_argument("--skip_training", action="store_true")
